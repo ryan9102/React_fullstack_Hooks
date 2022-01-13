bb79272d0291c0d42cc4299c2543d2fdca06d878
Hualiang <ryan_li@outlook.com.au>
2022-01-13 18:36:20 +1100

    adding sidedrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
-import MainHeader from './MainHeader'
-import './MainNavigation.css'
 
-export default function MainNavigation() {
+import MainHeader from './MainHeader'
+import NavLinks from './NavLinks'
+import SideDrawer from './SideDrawer'
+
+import './MainNavigation.css'
+import Backdrop from '../UIElements/Backdrop'
+
+export default function MainNavigation(props) {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+    
+    const openDrawer = () => {
+        setDrawerIsOpen(true)
+    }
+
+    const closeDrawer = ()=> {
+        setDrawerIsOpen(false)
+    }
+
     return( 
+        <React.Fragment>
+        {drawerIsOpen? <Backdrop onClick= {closeDrawer} />: null}
+       
+        <SideDrawer show={drawerIsOpen}>
+            <nav className='main-navigation__drawer-nav'>
+                <NavLinks/>
+            </nav>
+        </SideDrawer>
+        
+
         <MainHeader>
-            <button className='main-navigation__menu-btn'>
+            <button className='main-navigation__menu-btn' onClick={openDrawer}>
                 <span />
                 <span />
                 <span />
@@ -14,9 +39,10 @@ export default function MainNavigation() {
             <h1 className='main-navigation__title'>
                 <Link to='/'>YourPlaces</Link> 
             </h1>
-            <nav>
-                ...
+            <nav className='main-navigation__header-nav'>
+                <NavLinks/>
             </nav>
         </MainHeader>
+        </React.Fragment>
     )
 }
