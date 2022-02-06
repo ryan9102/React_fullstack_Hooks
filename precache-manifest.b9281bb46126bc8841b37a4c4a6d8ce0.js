cb53798618655f8ca2ca92bce1c264b2c1de9c27
Hualiang <ryan_li@outlook.com.au>
2022-02-06 15:46:28 +1100

    Updates

---
+++ precache-manifest.b9281bb46126bc8841b37a4c4a6d8ce0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6522c814218c19c495a46de22c249d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42c1efaf7b05ee68f7e7",
+    "url": "./static/css/main.bb168242.chunk.css"
+  },
+  {
+    "revision": "29e92e973615e3833f57",
+    "url": "./static/js/2.fd15894b.chunk.js"
+  },
+  {
+    "revision": "42c1efaf7b05ee68f7e7",
+    "url": "./static/js/main.dd1a9613.chunk.js"
+  },
+  {
+    "revision": "180de51469398a5d5462",
+    "url": "./static/js/runtime-main.bbe4aa2b.js"
+  }
+]);
