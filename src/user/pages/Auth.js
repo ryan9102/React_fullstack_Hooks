2cabacc676f6c5504785f37934240f6fe3dc1759
Hualiang <ryan_li@outlook.com.au>
2022-04-13 22:26:36 +1000

    add place image upload

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -116,7 +116,12 @@ const Auth = () => {
             />
           )}
           {!isLoginMode && (
-            <ImageUpload center id="image" onInput={inputHandler} />
+            <ImageUpload
+              center
+              id="image"
+              onInput={inputHandler}
+              errorText="Please provide an image."
+            />
           )}
           <Input
             element="input"
